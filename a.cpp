4f6e1207abd32a6e62658a74b41b8b7335855d44
Akshita <akshitajain207@gmail.com>
2019-10-03 01:24:23 +0530

    first

---
+++ a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t=100;
+    cout<<t;
+    while(t--){
+        int r=10;
+        int n=(rand()%r)+1;
+        int m=(rand()%r)+1;
+        cout<<n<<" "<<m<<endl;
+    }
+}
+
+
+
+
+
+
